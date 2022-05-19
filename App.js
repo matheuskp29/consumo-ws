1b111915d9796131745c78e45c53265cc4cc5631
Matheus Kaczan <matheuskp29@gmail.com>
2022-05-19 19:18:08 -0300

    Adicionando os useStates

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { APPID, BASE_URL, CNT, LANGUAGE, PROTOCOL, UNITS } from '@env';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const [cidade, setCidade] = useState('');
+    const [previsoes, setPrevisoes] = useState([]);
+
+    const capturarCidade = (cidadeDigitada) => {
+        setCidade(cidadeDigitada);
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
